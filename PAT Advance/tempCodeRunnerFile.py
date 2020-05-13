e42e10bb5698f72c1562e168fc5c6c4ec3b99567
Francis <francis.linking@gmail.com>
2020-05-13 20:28:31 +0800

    temp

--- PAT Advance/tempCodeRunnerFile.py
+++ PAT Advance/tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-LevelTraversal
+N, L = map(int,input().split())
+
+LGraph = [ [] for i in range(N+1)]
+
+for item in range(1,N+1):
+    inputLine = list(map(int,input().split()))
+    if inputLine[0] != 0:
+        for user in inputLine[1::]:
+            LGraph[user].append(item)
+
+UserID = list(map(int,input().split()))
